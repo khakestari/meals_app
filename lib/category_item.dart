f75f05d2c843a49d426fd491b1d3a758a52daf4f
Mostafa Nematollah zade <mostafanmz@yahoo.com>
2023-06-14 02:41:23 +0330

    add home page, fonts & theme data

---
+++ lib/category_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headlineLarge,
+      ),
+    );
+  }
+}
